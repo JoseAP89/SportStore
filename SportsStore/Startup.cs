bdf349ae994b5dbf2b38414a7fa042d33e7e79ba
Jose APumarino <josxe89@gmail.com>
2021-04-03 10:30:42 -0600

    Chapter 7

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,15 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public Startup(IConfiguration config) {
+            Configuration = config;
+        }
+        private IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts => {
+                opts.UseSqlServer(
+                    Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +39,18 @@ namespace SportsStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("pagination",
+                    "Products/Page{productPage}",
+                    new { Controller = "Home", action = "Index" });
+                endpoints.MapDefaultControllerRoute();
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
